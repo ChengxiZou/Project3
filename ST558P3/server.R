fe028ec2af67a90607018985de8c3d5ad564a91c
ChengxiZou <czou2@ncsu.edu>
2022-07-30 23:57:44 -0400

    flower pics and download link

--- ST558P3/server.R
+++ ST558P3/server.R
@@ -8,47 +8,30 @@
 #
 
 library(shiny)
+library(ggplot2)
+library(dplyr)
 library(shinydashboard)
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
-  
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
-    
     SpeciesData <- reactive({
       v <- input$v
-      
       irisSpeciesData <- iris %>% filter(Species == v)
       irisSpeciesData
-    })
+      })
     
-    url <- a("Google Homepage", href="https://www.google.com/")
-    output$tab <- renderUI({
-      paste("URL link:", url)
-    })
-    
-    # output$info <- renderText({
-      #get data
-      # SpeciesData <- SpeciesData()
-    # })
-    
+    # table
     output$table <- renderTable({
-      #get data
       SpeciesData <- SpeciesData()
       SpeciesData
     })
     
      output$downloadData <- downloadHandler(
        filename = function() {
-         paste('irisSpeciesData', Sys.Date(), '.csv', sep='')
+         paste(input$v, Sys.Date(), '.csv', sep='')
        },
-       content = function(SpeciesData) {
-         write.csv(data, SpeciesData)
+       content = function(file) {
+         write.csv(SpeciesData(),file)
        }
      )
     
-  })
-}
+  }
--- ST558P3/ui.R
+++ ST558P3/ui.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(dplyr)
 library(shinydashboard)
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
@@ -15,7 +16,7 @@ ui <- dashboardPage(
   ## Sidebar content
   dashboardSidebar(
     sidebarMenu(
-      menuItem("About", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("About", tabName = "dashboard", icon = icon("th")),
       menuItem("Data Exploration", tabName = "widgets", icon = icon("th")),
       menuItem("Modeling", tabName = "Modeling", icon = icon("th")),
       menuItem("Data", tabName = "Data", icon = icon("th"))
@@ -25,24 +26,54 @@ ui <- dashboardPage(
     tabItems(
       # First tab content
       tabItem(tabName = "dashboard",
-              fluidRow(h3("∗ Describe the purpose of the app"),br(),
+              fluidRow(h3("The purpose of the app"),br(),
                        h4("This app is specially designed for new R learners to get a better feeling of the iris data set
                        that is often used when subsetting data, creating plots or doing analysis on with R."),br(),
-                       h3("∗ Briefly discuss the data and its source - providing a link to more information about the data"),br(),
+                       
+                       h3("The data and its source - and a link to more information about it"),br(),
+                       
                        h4("This famous (Fisher's or Anderson's) iris data set gives the measurements in centimeters
                        of the variables sepal length and width and petal length and width, respectively,
                        for 50 flowers from each of 3 species of iris. The species are Iris setosa,
                        versicolor, and virginica."),br(),
+                       
                        h4("A sepal is a part of the flower of angiosperms (flowering plants). Usually green,
                        sepals typically function as protection for the flower in bud, and often as support for
                        the petals when in bloom."),br(),
+                       
                        h4("Petals are modified leaves that surround the reproductive parts of flowers.
                           They are often brightly colored or unusually shaped to attract pollinators."),br(),
+                       
                        tags$img(src='p1.png'),br(),
-                       br(),a(href="https://www.statology.org/iris-dataset-r/", "For more infomation please go to this Complete Guide!"),br(),
-                       h3("∗ Tell the user the purpose of each tab (page) of the app"),br(),
-                       h4("3"),br(),
-                       h3("∗ Include a picture related to the data (for instance, if the data was about the world wildlife fund, you might include a picture of their logo)"),br()
+                       
+                       br(),a(href="https://www.statology.org/iris-dataset-r/", "For more infomation please
+                              go to this Complete Guide!"),br(),
+                       
+                       h3("The purpose of each tab (page) of the app"),br(),
+                       
+                       h4("This app has four tabs. The first is this About tab now you are in -- it describes the
+                          purpose of this app and some relevant knowledge about iris and flowers. I also added some
+                          good pictures!"),br(),
+                       
+                       h4("The second tab is for data exploration."),br(),
+                       
+                       h4("The third tab is for data modeling."),br(),
+                       
+                       h4("The fourth tab is for data browsing."),br(),
+                       
+                       h3("Related pictures"),br(),
+                       
+                       h3("setosa:"),br(),
+                       
+                       tags$img(src='setosa.png'),br(),
+                       
+                       h3("versicolor:"),br(),
+                       
+                       tags$img(src='versicolor.png'),br(),
+                       
+                       h3("virginica:"),br(),
+                       
+                       tags$img(src='virginica.png'),br(),
               )
       ),
       
